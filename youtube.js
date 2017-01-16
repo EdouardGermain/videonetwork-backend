96d8ffcf06a054c41e12ea1344a18f7e4912caa8
NicolasForget <forget@polytech.unice.fr>
2017-01-16 16:15:25 +0100

    add youtube search

---
+++ youtube.js
@@ -0,0 +1,16 @@
+var YouTube = require('youtube-node');
+
+var youTube = new YouTube();
+
+// Set the key (using google developer)
+youTube.setKey('AIzaSyB1OOSpTREs85WUMvIgJvLTZKye4BVsoFU');
+
+// Search with id/name, number and result
+youTube.search('Amixem', 1, function(error, result) {
+  if (error) {
+    console.log(error);
+  }
+  else {
+    console.log(JSON.stringify(result, null, 2));
+  }
+});
