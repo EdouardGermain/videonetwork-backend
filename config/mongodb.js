547dd3182e2109134965dc94bf200ee7d7605ea0
Edouard Germain <edouard.germain@gmail.com>
2017-01-30 14:42:18 +0100

    maj

--- config/mongodb.js
+++ config/mongodb.js
@@ -6,9 +6,6 @@ module.exports = function() {
     mongoose.Promise = global.Promise;
     //mongoose.connect('mongodb://51.254.201.1:27017/videonetwork',
     mongoose.connect('mongodb://localhost:27017/videonetwork',
-        {server: {reconnectTries: Number.MAX_VALUE,
-            ssl: true,
-            sslValidate: true,
-            sslCA: CA}})
+        {server: {reconnectTries: Number.MAX_VALUE}})
 };
 
