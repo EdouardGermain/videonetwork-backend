4698ff3998901ff6af17f09bd513980bb47af774
Edouard Germain <edouard.germain@gmail.com>
2017-01-30 14:34:13 +0100

    add multi access origin site

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,8 +1,14 @@
 var mongoose = require('mongoose');
+const FS = require('fs');
+const CA = FS.readFileSync(__dirname + '/mongodb.pem');
 
 module.exports = function() {
     mongoose.Promise = global.Promise;
+    //mongoose.connect('mongodb://51.254.201.1:27017/videonetwork',
     mongoose.connect('mongodb://localhost:27017/videonetwork',
-        {server: {reconnectTries: Number.MAX_VALUE}});
+        {server: {reconnectTries: Number.MAX_VALUE,
+            ssl: true,
+            sslValidate: true,
+            sslCA: CA}})
 };
 
--- config/serveur.js
+++ config/serveur.js
@@ -6,7 +6,12 @@ var bodyParser = require('body-parser');
 module.exports = function(app,passport) {
 
     var allowCrossDomain = function(req, res, next) {
-        res.header('Access-Control-Allow-Origin', 'http://localhost');
+
+        var allowedOrigins = ['http://localhost', 'http://localhost:8080', 'http://127.0.0.1:8080', 'http://127.0.0.1'];
+        var origin = req.headers.origin;
+        if(allowedOrigins.indexOf(origin) > -1){
+            res.setHeader('Access-Control-Allow-Origin', origin);
+        }
         res.header('Access-Control-Allow-Credentials', true);
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Set-Cookie, Content-Type, Accept");
         next();
