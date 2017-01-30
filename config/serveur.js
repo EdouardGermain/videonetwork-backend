d3cf7f61f0d415336f400b65f844e168640cae58
Edouard Germain <edouard.germain@gmail.com>
2017-01-30 14:44:09 +0100

    maj

--- config/serveur.js
+++ config/serveur.js
@@ -10,7 +10,7 @@ module.exports = function(app,passport) {
         var allowedOrigins = ['http://localhost', 'http://localhost:8080', 'http://127.0.0.1:8080', 'http://127.0.0.1'];
         var origin = req.headers.origin;
         if(allowedOrigins.indexOf(origin) > -1){
-            res.setHeader('Access-Control-Allow-Origin', origin);
+            res.header('Access-Control-Allow-Origin', origin);
         }
         res.header('Access-Control-Allow-Credentials', true);
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Set-Cookie, Content-Type, Accept");
