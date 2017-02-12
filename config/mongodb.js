ba0ebcac9b223827d49c52c4b0aca87e7dda4841
Edouard Germain <edouard.germain@gmail.com>
2017-02-12 14:12:26 +0100

    mongo over ssh

--- config/mongodb.js
+++ config/mongodb.js
@@ -9,12 +9,15 @@
 
 
 var mongoose = require('mongoose');
-//const FS = require('fs');
-//const CA = FS.readFileSync(__dirname + '/mongodb.pem');
+const FS = require('fs');
+const CA = FS.readFileSync(__dirname + '/certificate/mongodb.pem');
 
 module.exports = function() {
     mongoose.Promise = global.Promise;
-    mongoose.connect('mongodb://edouardg.fr:27017/videonetwork',
-        {server: {reconnectTries: Number.MAX_VALUE}})
+    mongoose.connect('mongodb://localhost:27017/videonetwork?ssl=true',
+        {
+            sslValidate: true,
+            sslCA: CA,
+            server: {reconnectTries: Number.MAX_VALUE}})
 };
 
